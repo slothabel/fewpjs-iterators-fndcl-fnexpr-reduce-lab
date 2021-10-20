cde9a2d1c11ca824ab38c50d713ec17db5d5692c
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-20 16:06:32 -0400

    commit tree 759eda5fdeca5800140b0eb810e77d89206cf1a4

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const myReducer = (runningTotal, currentValue) => runningTotal + currentValue
+
+const totalBatteries = batteryBatches.reduce(myReducer, 0)
